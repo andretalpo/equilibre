4af81b890b2734e0e0bd5d09019fb1b3f4b6afef
Andre <andre.talpo@gmail.com>
2020-06-27 12:30:51 -0300

    Deploy do front

---
+++ public/precache-manifest.a0dec38ee3b6d18fe02ed2161a9d17f7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b597b5c5c4657496b95cec29e521e464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199840ec4b93de8b27e6",
+    "url": "/static/css/main.2e3bc1fa.chunk.css"
+  },
+  {
+    "revision": "cbad169dcecc7db69e0a",
+    "url": "/static/js/2.5b161ea4.chunk.js"
+  },
+  {
+    "revision": "64a4a56de25b0ab74ecfd443424fee83",
+    "url": "/static/js/2.5b161ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199840ec4b93de8b27e6",
+    "url": "/static/js/main.e4f06f8f.chunk.js"
+  },
+  {
+    "revision": "00463328046fbcbeffbb",
+    "url": "/static/js/runtime-main.468ab376.js"
+  },
+  {
+    "revision": "1a38e0c0894ab36a2dcbb68530054195",
+    "url": "/static/media/app-store-google.1a38e0c0.png"
+  },
+  {
+    "revision": "ebf359397b23d837a30b9e0476805cef",
+    "url": "/static/media/authPage.ebf35939.jpg"
+  },
+  {
+    "revision": "e7b0a951d9d629a7f4ba762e304e81c5",
+    "url": "/static/media/grid1.e7b0a951.jpg"
+  },
+  {
+    "revision": "99c1b78bb9a3e42d332ba3c5f3fc3847",
+    "url": "/static/media/grid2.99c1b78b.jpg"
+  },
+  {
+    "revision": "c1f1237691abf55f664d227b416881e5",
+    "url": "/static/media/grid3.c1f12376.jpg"
+  },
+  {
+    "revision": "ae8c03b79b7bb98028ead7001527f709",
+    "url": "/static/media/grid4.ae8c03b7.jpg"
+  },
+  {
+    "revision": "4ffd71325e0765b647be1b7a81f4b9a8",
+    "url": "/static/media/landingPage-1.4ffd7132.jpg"
+  },
+  {
+    "revision": "edd5318d651a09b6a6301f75cba5ae34",
+    "url": "/static/media/logo8.edd5318d.png"
+  }
+]);
